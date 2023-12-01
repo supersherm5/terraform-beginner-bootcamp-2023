130605c4ee3b5735d6f65541693a1154849ad789
Sherm Newsom <supersherm5@gmail.com>
2023-12-01 06:03:07 +0000

    use `~/.bash_profile` instead of `~/.bash_aliases

--- bin/load_terraform_alias.sh
+++ bin/load_terraform_alias.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 # Check to see if the alias exists
-grep -q 'alias tf="terraform"' ~/.bash_aliases 
+grep -q 'alias tf="terraform"' ~/.bash_profile
 
 # $? is as special variable in bash that holds the exit status of the last line completed
 if [[ $? -ne 0 ]]; then
     # If the alias doesn't exist, append it
-    echo 'alias tf="terraform"' >> ~/.bash_aliases
-    source ~/.bash_aliases # Refreshes session so, the alias will work
+    echo 'alias tf="terraform"' >> ~/.bash_profile
+    #source ~/.bash_aliases # Refreshes session so, the alias will work
     echo "Terraform Alias added."
 else
     # In form the user the terraform alias exists already.
