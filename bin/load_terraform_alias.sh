b742f5a26c32bd02ef13814fee4ea980145d05f2
Sherm Newsom <supersherm5@gmail.com>
2023-12-01 06:24:49 +0000

    tidy up the script

--- bin/load_terraform_alias.sh
+++ bin/load_terraform_alias.sh
@@ -6,10 +6,19 @@ grep -q 'alias tf="terraform"' ~/.bash_profile
 # $? is as special variable in bash that holds the exit status of the last line completed
 if [[ $? -ne 0 ]]; then
     # If the alias doesn't exist, append it
-    echo 'alias tf="terraform"' >> ~/.bash_profile
-    source ~/.bash_profile # Refreshes session so, the alias will work
+    echo -e '\nalias tf="terraform"' >> ~/.bash_profile
     echo "Terraform Alias added."
 else
     # In form the user the terraform alias exists already.
     echo "Terraform alias, exists already."
 fi
+
+# Check if the "tf" alias works
+if tf; then
+    echo "Terraform shortcut is ready to use."
+else
+    echo "Terraform shortcut is exists, but not ready. Refreshing the session." 
+    source ~/.bash_profile # Refreshes session so, the alias will work    
+    echo "Refresh complete."
+fi
+    
